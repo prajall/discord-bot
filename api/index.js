d2335320a5833b3d2114819534d681cdbb4e17bc
Prajal Maharjan <prajalmhrzn@gmail.com>
2024-11-10 22:15:02 +0545

    deploy

---
+++ api/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import dotenv from "dotenv";
+import { sendMessage } from "../bot.js";
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+sendMessage("First message from server");
+
+app.get("/", (req, res) => {
+  sendMessage("Hello from the API / route");
+  res.send("Discord Bot Server is Running!");
+});
+
+app.post("/send-message", (req, res) => {
+  const { message } = req.body;
+  console.log("Received message:", message);
+  const success = sendMessage(message);
+  return res.send(success ? "Message sent!" : "Failed to send message");
+});
